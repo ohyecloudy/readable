54c4041b8372865fb6c4b2ce666be61b632a3d37
ohyecloudy <ohyecloudy@gmail.com>
2013-08-24 17:17:08 +0900

    #1 프로젝트 세팅 및 환경 구성 > leiningen 프로젝트 세팅. index.html에서 빌드 결과물인 main.js 사용

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject readable "0.1.0-SNAPSHOT"
+  :description "가독성 테스트 웹페이지"
+  :url "https://github.com/ohyecloudy/readable"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-cljsbuild "0.3.2"]]
+  :cljsbuild {:builds [{:source-paths ["src-cljs"]
+                        :compiler {:output-to "js/main.js"
+                                   :optimizations
+                                   :whitespace
+                                   :pretty-print true}}]})
